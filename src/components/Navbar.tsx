cdb9b128906fa50fc45ff50a099ed7ed6164e6aa
Grace Gao <gracegao@Graces-MacBook-Pro.local>
2023-04-05 17:45:38 -0400

    fixed bug and able to pass prop through link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-      <Link to="/home"> Home</Link>
-      <Link to="/singleClient"> SingleClient</Link>
+      <Link to="/"> Home</Link>
+      {/* <Link to="/singleClient"> SingleClient</Link> */}
     </div>
   )
 }
--- src/components/SingleClient.tsx
+++ src/components/SingleClient.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useParams } from 'react-router-dom'
+import { Client, clients } from './ClientData'
 
 const SingleClient = () => {
-  return (
+// let {currentId}=useParams()
+const location=useLocation()
+const {name} = location.state
+let [currentClient, setCurrentClient]=useState<Client | undefined>(undefined)
+// function findClient(client:Client){
+//     if (client.id.toString()==currentId)
+//     setCurrentClient(client)
+// } 
+useEffect(() => {
+setCurrentClient(name)
+
+}, [])
+console.log(currentClient)
+return (
     <div>SingleClient
 
 
-        <div> TESTING </div>
+        <div>{currentClient?.name} </div>
+        <div> Balance: ${currentClient?.balance.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+  <div> Status: {currentClient?.isActiveClient? 'Active' : 'Not active'}</div>
     </div>
   )
 }
